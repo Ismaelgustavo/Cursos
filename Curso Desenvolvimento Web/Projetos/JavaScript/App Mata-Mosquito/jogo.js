2886b3dd962ad45c6e49bd97e45982eeec2a378e
Ismael Araujo <ismael.gustavo2471@gmail.com>
2022-10-10 10:54:22 -0300

    Game Mata Mosquito

---
+++ Curso Desenvolvimento Web/Projetos/JavaScript/App Mata-Mosquito/jogo.js
@@ -0,0 +1,88 @@
+const mosca = document.getElementById('mosquito1')
+const criaImg = document.createElement('img')
+
+
+
+let altura = 0
+let largura = 0
+let contador = 0
+vidas = 1
+
+let level = window.location.search
+level = level.replace('?','')
+level = Number(level)
+
+
+
+let timer = setInterval( function() {
+    posiçaoAleatoriaImg()
+    if(contador > 1 ){
+        document.getElementById('img'+ vidas).src = 'imagens/coracao_vazio.png' 
+        vidas++   
+    }
+},level);
+
+if (contador === 3){
+    
+}
+
+function ajustaTamanhoPalcoJogo(){
+    altura = window.innerHeight        
+    largura = window.innerWidth
+    console.log(altura,largura)
+}
+
+ajustaTamanhoPalcoJogo()
+
+
+
+// gera posiçao aleatória da imagem
+function posiçaoAleatoriaImg(){
+    contador++
+    let posicaoX = Math.floor(Math.random() * largura) - 90
+    let posicaoy = Math.floor(Math.random() * altura) - 90
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoy = posicaoy < 0 ? 0 : posicaoy
+    
+    console.log(posicaoX,posicaoy)
+        
+        criaImg.src ='imagens/mosca.png'
+        criaImg.className = tamanhoAleatorio()
+        criaImg.style.left = posicaoX  + 'px'
+        criaImg.style.top = posicaoy + 'px'
+        criaImg.style.position =  'absolute'
+        criaImg.setAttribute('onclick','disparaConsole()')
+        criaImg.onclick = function (){
+            this.remove()
+            contador = 0
+        }
+        document.body.appendChild(criaImg)
+        
+
+        
+
+
+}
+
+function tamanhoAleatorio(){
+    let classe = Math.floor(Math.random() * 3)
+
+    switch(classe){
+        case 0:
+            return 'mosquito1'
+        
+        case 1:
+            return 'mosquito2'
+            
+        case 2:
+            return 'mosquito3'    
+    }
+}
+
+
+
+
+
+
+
--- Curso JavaScript/Exercícios/Timer/script.js
+++ Curso JavaScript/Exercícios/Timer/script.js
@@ -1,8 +1,67 @@
 
+let segundo = document.querySelector('#segundo')
+let minuto = document.querySelector('#minuto')
+let play = document.querySelector('#iniciar')
+let pause = document.querySelector('#pausar')
+let restart = document.querySelector('#zerar')
+let clock = document.getElementById('relogio')
+
+play.addEventListener('click', startClock)
+pause.addEventListener('click', pauseClock)
+restart.addEventListener('click',restartClock)
+
+let sec = 0
+let mnts = 0
+let timer
+
+function startClock(){
+    clock.style.color = 'black'
+    timer = setInterval(function() {
+        sec++
+        segundo.innerHTML = `0${sec}`
+
+        if (sec === 60){
+            
+            segundo.innerHTML = "00"
+            
+            sec = 0
+            mnts++
+
+            minuto.innerHTML= `0${mnts}:`
+        }
+        zeroAEsquerda()
+        clock.style.color = 'black'
+
+    
+
+    }, 1000);
 
 
+}
 
+function pauseClock() {
 
+    clearInterval(timer)
+    clock.style.color = 'red'
+    
+}
 
+function restartClock(){
+    clearInterval(timer)
+    segundo.innerHTML = "00"
+    minuto.innerHTML = '00:'
+    clock.style.color = 'black'
+}
 
+function zeroAEsquerda(){
+    if (sec > 9 ){
+        segundo.innerHTML = `${sec}`
+    }
+
+    if ( mnts > 9  ){
+        minuto.innerHTML = `${sec}`
+    }
+  
+   
+}
 
